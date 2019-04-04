1db2b139c1887b5de255b89506e46ff8806491f0
Steven Rogalsky <srogalsky8@gmail.com>
2019-04-04 12:03:09 -0700

    Demonstrating this inside objects and functions for arrow functions

---
+++ arrow-functions/this.js
@@ -0,0 +1,26 @@
+class Tester {
+    constructor() {
+        this.x = 1
+        this.getX = () => this.x
+    }
+}
+
+let test = new Tester()
+console.log(test.getX()) // 1 - uses this of constructor()
+console.log(test)
+
+let test2 = {
+    x: 1,
+    getX: () => this.x
+}
+console.log(test2.getX()) // undefined - uses global this
+console.log(test2)
+
+function Tester3() {
+    this.x = 1
+    this.getX = () => this.x 
+}
+
+let test3 = new Tester3()
+console.log(test3.getX()) // 1 - uses this of Tester3()
+console.log(test3)
